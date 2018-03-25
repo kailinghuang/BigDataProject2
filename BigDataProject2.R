f2d2468ca0f92494a32caaeabec026e0767f8c54
AlexQianYi <q563875735@gmail.com>
2018-03-25 17:15:12 -0400

    demo

--- BigDataProject2.R
+++ BigDataProject2.R
@@ -13,6 +13,15 @@ names(mr)<-c('class', 'cshape', 'csurface', 'ccolor', 'bruises', 'odor', 'gattac
              'rnumber', 'rtype', 'spcolor', 'popnum', 'habitat')
 names(mr)
 
+## training-test dataset
+train.df<-sample(nrow(mr), 0.7*nrow(mr))
+#train.df<-sample(nrow(mr), 0.6*nrow(mr))
+#train.df<-sample(nrow(mr), 0.5*nrow(mr))
+
+#train set
+mrtrain.df<-mr[train.df,]
+#test set
+mrtest.df<-mr[-train.df,]
 
 
 mrf<-as.data.frame(mr,stringAsFactors=FALSE)
@@ -47,127 +56,147 @@ mrf$ccolor[mrf$ccolor=='e']<-15
 mrf$ccolor[mrf$ccolor=='w']<-17
 mrf$ccolor[mrf$ccolor=='y']<-19
 # bruises
-mrf$class[mrf$bruises=='t']<-1
-mrf$class[mrf$bruises=='f']<-3
+mrf$bruises<-as.character(mrf$bruises)
+mrf$bruises[mrf$bruises=='t']<-1
+mrf$bruises[mrf$bruises=='f']<-3
 # odor
-mrf$class[mrf$odor=='a']<-1
-mrf$class[mrf$odor=='l']<-3
-mrf$class[mrf$odor=='c']<-5
-mrf$class[mrf$odor=='y']<-7
-mrf$class[mrf$odor=='f']<-9
-mrf$class[mrf$odor=='m']<-11
-mrf$class[mrf$odor=='n']<-13
-mrf$class[mrf$odor=='p']<-15
-mrf$class[mrf$odor=='s']<-17
+mrf$odor<-as.character(mrf$odor)
+mrf$odor[mrf$odor=='a']<-1
+mrf$odor[mrf$odor=='l']<-3
+mrf$odor[mrf$odor=='c']<-5
+mrf$odor[mrf$odor=='y']<-7
+mrf$odor[mrf$odor=='f']<-9
+mrf$odor[mrf$odor=='m']<-11
+mrf$odor[mrf$odor=='n']<-13
+mrf$odor[mrf$odor=='p']<-15
+mrf$odor[mrf$odor=='s']<-17
 # gattach
-mrf$class[mrf$gattach=='a']<-1
-mrf$class[mrf$gattach=='d']<-3
-mrf$class[mrf$gattach=='f']<-5
-mrf$class[mrf$gattach=='n']<-7
+mrf$gattach<-as.character(mrf$gattach)
+mrf$gattach[mrf$gattach=='a']<-1
+mrf$gattach[mrf$gattach=='d']<-3
+mrf$gattach[mrf$gattach=='f']<-5
+mrf$gattach[mrf$gattach=='n']<-7
 # gspace
-mrf$class[mrf$gspace=='c']<-1
-mrf$class[mrf$gspace=='d']<-3
-mrf$class[mrf$gspace=='f']<-5
-mrf$class[mrf$gspace=='n']<-7
+mrf$gspace<-as.character(mrf$gspace)
+mrf$gspace[mrf$gspace=='c']<-1
+mrf$gspace[mrf$gspace=='d']<-3
+mrf$gspace[mrf$gspace=='f']<-5
+mrf$gspace[mrf$gspace=='n']<-7
 # gsize
-mrf$class[mrf$gsize=='b']<-1
-mrf$class[mrf$gsize=='n']<-3
+mrf$gsize<-as.character(mrf$gsize)
+mrf$gsize[mrf$gsize=='b']<-1
+mrf$gsize[mrf$gsize=='n']<-3
 # gcolor
-mrf$class[mrf$gcolor=='k']<-1
-mrf$class[mrf$gcolor=='n']<-3
-mrf$class[mrf$gcolor=='b']<-5
-mrf$class[mrf$gcolor=='h']<-7
-mrf$class[mrf$gcolor=='g']<-9
-mrf$class[mrf$gcolor=='r']<-11
-mrf$class[mrf$gcolor=='o']<-13
-mrf$class[mrf$gcolor=='p']<-15
-mrf$class[mrf$gcolor=='u']<-17
-mrf$class[mrf$gcolor=='e']<-19
-mrf$class[mrf$gcolor=='w']<-21
-mrf$class[mrf$gcolor=='y']<-23
+mrf$gcolor<-as.character(mrf$gcolor)
+mrf$gcolor[mrf$gcolor=='k']<-1
+mrf$gcolor[mrf$gcolor=='n']<-3
+mrf$gcolor[mrf$gcolor=='b']<-5
+mrf$gcolor[mrf$gcolor=='h']<-7
+mrf$gcolor[mrf$gcolor=='g']<-9
+mrf$gcolor[mrf$gcolor=='r']<-11
+mrf$gcolor[mrf$gcolor=='o']<-13
+mrf$gcolor[mrf$gcolor=='p']<-15
+mrf$gcolor[mrf$gcolor=='u']<-17
+mrf$gcolor[mrf$gcolor=='e']<-19
+mrf$gcolor[mrf$gcolor=='w']<-21
+mrf$gcolor[mrf$gcolor=='y']<-23
 # sshape
-mrf$class[mrf$sshape=='e']<-1
-mrf$class[mrf$sshape=='t']<-3
+mrf$sshape<-as.character(mrf$sshape)
+mrf$sshape[mrf$sshape=='e']<-1
+mrf$sshape[mrf$sshape=='t']<-3
 # sroot
-mrf$class[mrf$sroot=='b']<-1
-mrf$class[mrf$sroot=='c']<-3
-mrf$class[mrf$sroot=='u']<-5
-mrf$class[mrf$sroot=='e']<-7
-mrf$class[mrf$sroot=='z']<-9
-mrf$class[mrf$sroot=='r']<-11
-mrf$class[mrf$sroot=='?']<-13
+mrf$sroot<-as.character(mrf$sroot)
+mrf$sroot[mrf$sroot=='b']<-1
+mrf$sroot[mrf$sroot=='c']<-3
+mrf$sroot[mrf$sroot=='u']<-5
+mrf$sroot[mrf$sroot=='e']<-7
+mrf$sroot[mrf$sroot=='z']<-9
+mrf$sroot[mrf$sroot=='r']<-11
+mrf$sroot[mrf$sroot=='?']<-13
 # ssabove
-mrf$class[mrf$ssabove=='f']<-1
-mrf$class[mrf$ssabove=='y']<-3
-mrf$class[mrf$ssabove=='k']<-5
-mrf$class[mrf$ssabove=='s']<-7
+mrf$ssabove<-as.character(mrf$ssabove)
+mrf$ssabove[mrf$ssabove=='f']<-1
+mrf$ssabove[mrf$ssabove=='y']<-3
+mrf$ssabove[mrf$ssabove=='k']<-5
+mrf$ssabove[mrf$ssabove=='s']<-7
 # ssbelow
-mrf$class[mrf$ssbelow=='f']<-1
-mrf$class[mrf$ssbelow=='y']<-3
-mrf$class[mrf$ssbelow=='k']<-5
-mrf$class[mrf$ssbelow=='s']<-7
+mrf$ssbelow<-as.character(mrf$ssbelow)
+mrf$ssbelow[mrf$ssbelow=='f']<-1
+mrf$ssbelow[mrf$ssbelow=='y']<-3
+mrf$ssbelow[mrf$ssbelow=='k']<-5
+mrf$ssbelow[mrf$ssbelow=='s']<-7
 # scabove
-mrf$class[mrf$scabove=='n']<-1
-mrf$class[mrf$scabove=='b']<-3
-mrf$class[mrf$scabove=='c']<-5
-mrf$class[mrf$scabove=='g']<-7
-mrf$class[mrf$scabove=='o']<-9
-mrf$class[mrf$scabove=='p']<-11
-mrf$class[mrf$scabove=='e']<-13
-mrf$class[mrf$scabove=='w']<-15
-mrf$class[mrf$scabove=='y']<-17
+mrf$scabove<-as.character(mrf$scabove)
+mrf$scabove[mrf$scabove=='n']<-1
+mrf$scabove[mrf$scabove=='b']<-3
+mrf$scabove[mrf$scabove=='c']<-5
+mrf$scabove[mrf$scabove=='g']<-7
+mrf$scabove[mrf$scabove=='o']<-9
+mrf$scabove[mrf$scabove=='p']<-11
+mrf$scabove[mrf$scabove=='e']<-13
+mrf$scabove[mrf$scabove=='w']<-15
+mrf$scabove[mrf$scabove=='y']<-17
 # scbelow
-mrf$class[mrf$scbelow=='n']<-1
-mrf$class[mrf$scbelow=='b']<-3
-mrf$class[mrf$scbelow=='c']<-5
-mrf$class[mrf$scbelow=='g']<-7
-mrf$class[mrf$scbelow=='o']<-9
-mrf$class[mrf$scbelow=='p']<-11
-mrf$class[mrf$scbelow=='e']<-13
-mrf$class[mrf$scbelow=='w']<-15
-mrf$class[mrf$scbelow=='y']<-17
+mrf$scbelow<-as.character(mrf$scbelow)
+mrf$scbelow[mrf$scbelow=='n']<-1
+mrf$scbelow[mrf$scbelow=='b']<-3
+mrf$scbelow[mrf$scbelow=='c']<-5
+mrf$scbelow[mrf$scbelow=='g']<-7
+mrf$scbelow[mrf$scbelow=='o']<-9
+mrf$scbelow[mrf$scbelow=='p']<-11
+mrf$scbelow[mrf$scbelow=='e']<-13
+mrf$scbelow[mrf$scbelow=='w']<-15
+mrf$scbelow[mrf$scbelow=='y']<-17
 # vtype
-mrf$class[mrf$vtype=='p']<-1
-mrf$class[mrf$vtype=='u']<-3
+mrf$vtype<-as.character(mrf$vtype)
+mrf$vtype[mrf$vtype=='p']<-1
+mrf$vtype[mrf$vtype=='u']<-3
 # vcolor
-mrf$class[mrf$vcolor=='n']<-1
-mrf$class[mrf$vcolor=='o']<-3
-mrf$class[mrf$vcolor=='w']<-5
-mrf$class[mrf$vcolor=='y']<-7
+mrf$vcolor<-as.character(mrf$vcolor)
+mrf$vcolor[mrf$vcolor=='n']<-1
+mrf$vcolor[mrf$vcolor=='o']<-3
+mrf$vcolor[mrf$vcolor=='w']<-5
+mrf$vcolor[mrf$vcolor=='y']<-7
 # rnumber
-mrf$class[mrf$rnumber=='n']<-1
-mrf$class[mrf$rnumber=='o']<-3
-mrf$class[mrf$rnumber=='t']<-5
+mrf$rnumber<-as.character(mrf$rnumber)
+mrf$rnumber[mrf$rnumber=='n']<-1
+mrf$rnumber[mrf$rnumber=='o']<-3
+mrf$rnumber[mrf$rnumber=='t']<-5
 # rtype
-mrf$class[mrf$rtype=='c']<-1
-mrf$class[mrf$rtype=='e']<-3
-mrf$class[mrf$rtype=='f']<-5
-mrf$class[mrf$rtype=='l']<-7
-mrf$class[mrf$rtype=='n']<-9
-mrf$class[mrf$rtype=='p']<-11
-mrf$class[mrf$rtype=='s']<-13
-mrf$class[mrf$rtype=='z']<-15
+mrf$rtype<-as.character(mrf$rtype)
+mrf$rtype[mrf$rtype=='c']<-1
+mrf$rtype[mrf$rtype=='e']<-3
+mrf$rtype[mrf$rtype=='f']<-5
+mrf$rtype[mrf$rtype=='l']<-7
+mrf$rtype[mrf$rtype=='n']<-9
+mrf$rtype[mrf$rtype=='p']<-11
+mrf$rtype[mrf$rtype=='s']<-13
+mrf$rtype[mrf$rtype=='z']<-15
 # spcolor
-mrf$class[mrf$spcolor=='k']<-1
-mrf$class[mrf$spcolor=='n']<-3
-mrf$class[mrf$spcolor=='b']<-5
-mrf$class[mrf$spcolor=='h']<-7
-mrf$class[mrf$spcolor=='r']<-9
+mrf$spcolor<-as.character(mrf$spcolor)
+mrf$spcolor[mrf$spcolor=='k']<-1
+mrf$spcolor[mrf$spcolor=='n']<-3
+mrf$spcolor[mrf$spcolor=='b']<-5
+mrf$spcolor[mrf$spcolor=='h']<-7
+mrf$spcolor[mrf$spcolor=='r']<-9
 # popnum
-mrf$class[mrf$popnum=='a']<-1
-mrf$class[mrf$popnum=='c']<-3
-mrf$class[mrf$popnum=='n']<-5
-mrf$class[mrf$popnum=='s']<-7
-mrf$class[mrf$popnum=='v']<-9
-mrf$class[mrf$popnum=='y']<-11
+mrf$popnum<-as.character(mrf$popnum)
+mrf$popnum[mrf$popnum=='a']<-1
+mrf$popnum[mrf$popnum=='c']<-3
+mrf$popnum[mrf$popnum=='n']<-5
+mrf$popnum[mrf$popnum=='s']<-7
+mrf$popnum[mrf$popnum=='v']<-9
+mrf$popnum[mrf$popnum=='y']<-11
 # habitat
-mrf$class[mrf$habitat=='g']<-1
-mrf$class[mrf$habitat=='l']<-3
-mrf$class[mrf$habitat=='m']<-5
-mrf$class[mrf$habitat=='p']<-7
-mrf$class[mrf$habitat=='u']<-9
-mrf$class[mrf$habitat=='w']<-11
-mrf$class[mrf$habitat=='d']<-13
+mrf$habitat<-as.character(mrf$habitat)
+mrf$habitat[mrf$habitat=='g']<-1
+mrf$habitat[mrf$habitat=='l']<-3
+mrf$habitat[mrf$habitat=='m']<-5
+mrf$habitat[mrf$habitat=='p']<-7
+mrf$habitat[mrf$habitat=='u']<-9
+mrf$habitat[mrf$habitat=='w']<-11
+mrf$habitat[mrf$habitat=='d']<-13
+
 
 
 pairs(class~cshape+csurface+ccolor, data=mr)
